fc27ee1b27c3dc0f202bda7d70d679a99a92a251
Engin <engingltekin@gmail.com>
2022-01-18 22:01:36 -0500

    hands on flask project

---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def head():
+    return "Hello World!123"
+
+@app.route("/second")
+def second():
+    return "second page"
+
+@app.route("/third/engin")
+def third():
+    return "third page"
+
+@app.route("/fourth/<string:id>")
+def fourth(id):
+    return f"fourth page {id}"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/app.py
@@ -0,0 +1,20 @@
+from pipes import Template
+from flask import Flask, render_template
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def head():
+    return render_template("index.html", number1 = 34, number2 = 45)
+
+@app.route("/function")
+def function():
+    variable1 = 12
+    variable2 = 5
+    return render_template("body.html", num1 = variable1, num2= variable2, multp = variable1 * variable2)
+
+if __name__=="__main__":
+    app.run(debug=True)
