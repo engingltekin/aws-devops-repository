9bd7fa307510e26378671276d950e8f1ef93b07a
Engin <engingltekin@gmail.com>
2022-02-22 18:32:37 -0500

    Organize Python Code Challenges

--- Python-Handson/cc-005-create-phonebook/manage-phonebook.py
+++ Python-Handson/cc-005-create-phonebook/manage-phonebook.py
@@ -1,30 +1,11 @@
-'''
-Write a Python Program using text files  "C://Users//Engin-Didem//OneDrive//Desktop//phonebook.txt"
-to create and maintain a Phone Book.
-The Phone Book will display the
-following Menu and will support
-the corresponding functionality
-
-*** Phone Book Menu ***
-Welcome to the phonebook application
-1. Find phone number
-2. Insert a phone number
-3. Delete a person from the phonebook
-4. Terminate
-Select operation on Phonebook App (1/2/3) :
-**********************
-'''
 import re
-
-phone_match = re.compile("^(\w{3}-\w{3}-\w{4})$")#basic regex
 
 class PhoneInfo:
     def __init__(self, name, phoneNumber):
         self.name = name
         self.phoneNumber = phoneNumber
-
 phoneList = []
-
+phone_match = re.compile("^(\w{3}-\w{3}-\w{4})$")#basic regex
 
 def show_main_menu():
     ''' Show main menu for Phone Book Program '''
@@ -37,23 +18,17 @@ def show_main_menu():
     try:
         match choice:
             case "1":
-                #Check if the Phone book is Empty
-                #else find record from list and display phone number and information
                 if not phoneList:
                     print("Phone Book is empty")
                 else:
-                #Find
                     search_contact_record()
                 ent = input("Press Enter to continue ...")
                 show_main_menu()
-            #return to home menu after each operation is completed
             case "2":
-                #create contact record
                 enter_contact_record()
                 ent = input("Press Enter to continue ...")
                 show_main_menu()
             case "3":
-                #search and delete phone record
                 delete_contact_record()
                 ent = input("Press Enter to continue ...")
                 show_main_menu() 
@@ -68,10 +43,8 @@ def show_main_menu():
         ent = input("Press Enter to continue ...")
         show_main_menu() 
     #finally: For any clean up 
-                
     
 def search_contact_record():
-    ''' This function is used to searches a specific contact record '''
     search_name = input("Enter First name for Searching contact record: ")
     search_name = search_name.title()
     found = False   
@@ -84,8 +57,6 @@ def search_contact_record():
         print("There's no contact Record in Phone Book with name = " + search_name )
 
 def enter_contact_record():
-    ''' It  collects contact info name and phone '''
-   
     personName = input('Insert name of the person: ')
     personName = personName.title()
     phone = input('Please enter a phone number in the format XXX-XXX-XXXX: ')
@@ -109,9 +80,4 @@ def delete_contact_record():
     for phoneInfo in phoneList:
         if personName == phoneInfo.name:
             phoneList.remove(phoneInfo)
-
-
-          
-
-#Initiate the program
 show_main_menu()
