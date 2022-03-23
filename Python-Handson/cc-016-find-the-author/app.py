aef10b42b782445dc515fe4f371179a0d6dca969
Engin <engingltekin@gmail.com>
2022-03-23 00:13:47 -0400

    Find the author Pyhton exercise

---
+++ Python-Handson/cc-016-find-the-author/app.py
@@ -0,0 +1,4 @@
+f = open("C://Users//Engin-Didem//OneDrive//Desktop//aws-devops-repository//Python-Handson//cc-016-find-the-author//text1.txt",'rb')
+text1data = f.readlines()
+# do file operations.
+f.close()
